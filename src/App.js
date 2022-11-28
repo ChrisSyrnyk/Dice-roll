40f3787dda558ad035af45b392172fd63e0f090c
Christopher Syrnyk <christophersyrnyk@Christophers-Mac-mini.local>
2022-11-28 13:00:21 -0700

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './styles.css'
+import React, {useState} from 'react';
 
 function App() {
+
+  const [leftDieResult, setLeftDieResult] = useState(2);
+  const [rightDieResult, setRightDieResult] = useState(1);
+
+
+
+  const leftDieImg = require('./assets/'+leftDieResult+'.png');
+  const rightDieImg = require('./assets/'+rightDieResult+'.png');
+
+   //produce random number between min and max
+  function ranNum(min, max){
+    let rNum = Math.random() * (max-min) + min;
+    let rINum = parseInt(rNum);
+    return rINum;
+  };
+
+  function rollDice(){
+    setLeftDieResult(ranNum(1,7));
+    setRightDieResult(ranNum(1,7));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="gameContainer roundCorners">
+      <div className="diceContainer roundCorners">
+        <img src={leftDieImg} alt = "left die" className = 'die'/>
+        
+        <img src={rightDieImg} alt = "right die" className = 'die'/>
+        
+      </div>
+      <div className = 'scoreContainer'>
+        {leftDieResult + rightDieResult}
+      </div>
+      <div className = "buttonContainer">
+        <button className='buttonStyle' onClick = {()=>rollDice()}>Roll Dice</button>
+      </div>
     </div>
+    </>
   );
 }
 
